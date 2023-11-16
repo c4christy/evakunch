20a483734c712eb8298fc2a39f3be5146d0a8b94
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2023-11-16 23:06:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL)
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001668454819)
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001668454819))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'aml_admin')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
