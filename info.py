6411303f11767f4403cd16ef31df2183cf098f42
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2023-11-16 23:20:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS' ,'https://te.legra.ph/file/2536d5e8c9f5faa51b253.jpg https://te.legra.ph/file/08886eeeaa2958470559b.jpg https://te.legra.ph/file/38e1b2dea57ca07103697.jpg https://te.legra.ph/file/102528ab6013ddafc61d6.jpg https://te.legra.ph/file/5b576f96776c2af526982.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '683891378').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '683891378 5845960615').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001709366899 -1001838072998').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
