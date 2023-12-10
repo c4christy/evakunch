534d8c686075ae5cf0102b96647a216605593506
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2023-12-10 14:29:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ PICS = (environ.get('PICS' ,'https://te.legra.ph/file/2536d5e8c9f5faa51b253.jpg 
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '683891378 5845960615').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001709366899 -1001838072998').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001709366899 -1001838072998 -1002038316280').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
